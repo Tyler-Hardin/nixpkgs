1c4b367c1af8535ddbbbc7fc66ef2c1d1ae885df
Tyler Hardin <th020394@gmail.com>
2025-01-06 19:36:52 -0500

    amdgpu-i2c: init at 0.0.1
    This module exposes the i2c interfaces necessary for OpenRGB and similar
    tools to set colors on the GPU.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22979,6 +22979,12 @@
     githubId = 1141680;
     name = "Thane Gill";
   };
+  thardin = {
+    email = "th020394@gmail.com";
+    github = "Tyler-Hardin";
+    githubId = 5404976;
+    name = "Tyler Hardin";
+  };
   thblt = {
     name = "Thibault Polge";
     email = "thibault@thb.lt";
